6dd6274edf867839ec491ee7d2d23a7cb831a4b4
Boyan Ivanov <b.i.ivanov1@gmail.com>
2024-03-16 01:41:07 +0200

    fix: added assignees to table tasks

---
+++ prisma/migrations/20240315233940_tasks_assignees/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "_TaskToUser" (
+    "A" INTEGER NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_TaskToUser_AB_unique" ON "_TaskToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_TaskToUser_B_index" ON "_TaskToUser"("B");
+
+-- AddForeignKey
+ALTER TABLE "_TaskToUser" ADD CONSTRAINT "_TaskToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "tasks"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_TaskToUser" ADD CONSTRAINT "_TaskToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
