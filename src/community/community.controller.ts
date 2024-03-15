db0770ab6d38b58c363429d8a5348ab6dbc68342
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-03-15 20:15:22 +0200

    docs: add swagger annotations to community controller
    The community.controller.ts file has been modified to include Swagger annotations.
    Additions provide more information and description about the operation of getting all communities,
    enhancing API documentation visibility.

--- src/community/community.controller.ts
+++ src/community/community.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { CommunityService } from './community.service';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('communities')
+@ApiTags('Community API')
 export class CommunityController {
   constructor(private readonly communityService: CommunityService) {}
 
   @Get()
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    summary: 'Get all communities',
+    description: 'Endpoint for getting all communities.',
+  })
+  @ApiOkResponse({
+    description: 'Communities returned successfully.',
+  })
   async getCommunity() {
     return this.communityService.getCommunity();
   }
