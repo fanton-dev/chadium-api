2df5d1a0dc850a813ba9f39f2a28708278a76604
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-03-15 19:24:32 +0200

    feat: add Community model and relations in prisma schema
    In the Prisma schema, a new Community model has been added to represent community data.
    It includes information like name, description, and color.
    It also establishes "CommunityOwner" and "CommunityMembers" relations with the User model to
    represent community ownership and membership respectively.

---
+++ prisma/migrations/20240315170543_community_init/migration.sql
@@ -0,0 +1,33 @@
+-- CreateTable
+CREATE TABLE "communities" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "color" TEXT NOT NULL,
+    "owner_id" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "communities_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_CommunityMembers" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CommunityMembers_AB_unique" ON "_CommunityMembers"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CommunityMembers_B_index" ON "_CommunityMembers"("B");
+
+-- AddForeignKey
+ALTER TABLE "communities" ADD CONSTRAINT "communities_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CommunityMembers" ADD CONSTRAINT "_CommunityMembers_A_fkey" FOREIGN KEY ("A") REFERENCES "communities"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CommunityMembers" ADD CONSTRAINT "_CommunityMembers_B_fkey" FOREIGN KEY ("B") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
